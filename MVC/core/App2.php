e61de63cf3e7a7ede6a032116fbfcf413eebb681
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2021-07-23 07:57:09 +0700

    Update App2.php

--- MVC/core/App2.php
+++ MVC/core/App2.php
@@ -31,10 +31,10 @@
     }
     function UrlProcess()
     {
-        if(isset($_SERVER['PATH_INFO']))
+         if(isset($_GET["url"]))
         {
             
-            return explode("/",filter_var(trim($_SERVER['PATH_INFO'],"/")));
+            return explode("/",filter_var(trim($_GET["url"],"/")));
         }
     }
 }
