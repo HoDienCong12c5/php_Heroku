ece30a6046923f0bf886361793478b08c816eb29
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2021-07-23 23:45:59 +0700

    Update ProductRevenueView.php

--- MVC/views/Admin/ProductRevenueView.php
+++ MVC/views/Admin/ProductRevenueView.php
@@ -123,7 +123,7 @@ function currency_format($number, $suffix = '')
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="/Public/All/js/jQuery.js"></script>
+    <script src="Public/All/js/jQuery.js"></script>
     <script>
         function ViewRevenue() {
             $.post("Admin/ProductRevenue/Chartt",
