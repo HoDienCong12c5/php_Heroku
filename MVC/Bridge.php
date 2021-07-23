327722cab29f70b70f664c3f6c5239faf8f08e74
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2021-07-23 07:59:56 +0700

    Update Bridge.php

--- MVC/Bridge.php
+++ MVC/Bridge.php
@@ -5,5 +5,5 @@
     require_once "MVC/core/Controller.php"; 
     require_once "MVC/core/DB.php"; 
     require_once "MVC/controllers/Admin/UpDownPoint_Sale.php"; 
-    $myApp = new App();
+    $myApp = new App2();
 ?>
