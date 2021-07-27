de10a1d114de18a20abaa4be2c7ff1f627286c30
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2021-07-27 08:19:25 +0700

    Update EditWorkView.php

--- MVC/views/Admin/EditWorkView.php
+++ MVC/views/Admin/EditWorkView.php
@@ -13,23 +13,7 @@
         include "MVC/controllers/Admin/Master.php";
         $mt=new Master();
         $mt->index();
-        $id="";
-        $name="";
-        $price="";
-        $time="";
-        $img="";
-        $note="";
-        $idDe="";
-        if(isset($data['data'])){
-            $r=mysqli_fetch_array($data['data']);
-            $id=$r[0];
-            $img=$r[1];
-            $name=$r[2];
-            $note=$r[3];
-            $price=$r[4];
-            $time=$r[5];
-            $idDe=$r[6];
-        }
+       
     ?>
 
 </body>
